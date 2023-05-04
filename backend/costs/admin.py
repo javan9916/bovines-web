f7c7fac0a861076fff32dbfdae8cdd0bc1ac4cb2
javan9916 <jarayaporras@gmail.com>
2023-05-03 19:21:49 -0600

    update: changes for deploying react app on gh-pages

--- backend/costs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Cost, Category
-
-
-admin.site.register(Cost)
-admin.site.register(Category)
--- backend/diets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Diet, Supplement, DietSupplement, PriceHistory
-
-
-admin.site.register(Diet)
-admin.site.register(Supplement)
-admin.site.register(DietSupplement)
-admin.site.register(PriceHistory)
